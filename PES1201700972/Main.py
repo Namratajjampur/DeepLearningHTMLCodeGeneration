a1bd30612d3955894b8e55dde89a0db2f33c7406
Namrata R <37548876+Namratajjampur@users.noreply.github.com>
2020-04-21 23:12:29 +0530

    Update Main.py

--- PES1201700972/Main.py
+++ PES1201700972/Main.py
@@ -8,20 +8,19 @@ star_text = '<START> '
 image = load_val_images('try/')[0]
 img_tensor=np.expand_dims(np.array(image),0)
 img_tensor=np.array(img_tensor)
-predicted = '<START> '
 predicted='<START>'
 star_text = '<START>'
 with tf.Session() as sess:
     saver.restore(sess, "model.ckpt")
     for di in range(1000):
-        sequence = my_dateset.tokenizer.texts_to_sequences([star_text])
+        sequence = test_dataset.tokenizer.texts_to_sequences([star_text])
         decoder_input = np.array(sequence).reshape(-1,1)
         print(decoder_input)
         a = sess.run(output_gru2, feed_dict={im:img_tensor,caption_p:decoder_input})
         print(a)
         data=list(a[0][0])
         i=data.index(max(data))
-        word = word_for_id(i,my_dateset.tokenizer)
+        word = word_for_id(i,test_dataset.tokenizer)
         if word is None:
             continue
         predicted += word + ' '
